a5240bb10eef6eb50a79d650737692957f28c2ef
tami-cp0 <findtamilore>
2024-07-09 14:03:45 +0100

    re-implemented auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,33 +11,31 @@ class Auth:
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        Will be fully implemented later
+        Check which routes don't need authentication
         """
-        # if not (path or excluded_paths):
-        #     return True
+        if not (path or excluded_paths):
+            return True
 
-        # if path[-1] != '/':
-        #     path += '/'
+        if path[-1] != '/':
+            path += '/'
 
-        # if path in excluded_paths:
-        #     return False
+        if path in excluded_paths:
+            return False
 
-        # return True
-        return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
-        Will be fully implemented later
+        Checks if Authorization header is present.
         """
-        # if not request:
-        #     return None
+        if not request:
+            return None
 
-        # auth = request.headers.get("Authorization", None)
-        # if not auth:
-        #     return None
+        auth = request.headers.get("Authorization", None)
+        if not auth:
+            return None
 
-        # return auth
-        return None
+        return auth
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
