dfb5bbbd48e51e5e7429f1c3c0f069498c379808
tami-cp0 <findtamilore>
2024-07-09 15:02:33 +0100

    pycodestyle

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,8 +9,13 @@ class BasicAuth(Auth):
     """
     Basic authentication class.
     """
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
-        if not authorization_header or not isinstance(authorization_header, str) or not authorization_header.startswith('Basic '):
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        if (
+            not authorization_header
+            or not isinstance(authorization_header, str)
+            or not authorization_header.startswith('Basic ')
+           ):
             return None
-        
+
         return authorization_header.split(' ')[1]
