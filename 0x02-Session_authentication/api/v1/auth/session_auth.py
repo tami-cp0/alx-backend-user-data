0b8e3168ebe30d536e1fecb0f844510395f0eb09
tami-cp0 <findtamilore>
2024-07-19 11:05:46 +0100

    method update

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 Module for SessionAUTH class
 """
 from api.v1.auth.auth import Auth
+from models.user import User
 import uuid
 
 
@@ -33,3 +34,13 @@ class SessionAuth(Auth):
             return None
 
         return self.user_id_by_session_id.get(session_id, None)
+
+    def current_user(self, request=None):
+        """
+        Returns a User instance based on a cookie value.
+        """
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        user = User.get(user_id)
+
+        return user
